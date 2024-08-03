5cea23cd2bc3bd3a1acf13f97602bd9e647eb38e
Jahnavi Singh Chauhan <jahnavi.singhchauhan@studentambassadors.com>
2024-08-03 17:36:03 +0530

    Delete 1556-make-two-arrays-equal-by-reversing-subarrays directory

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public boolean canBeEqual(int[] target, int[] arr) {
-        Arrays.sort(target);
-        Arrays.sort(arr);
-        
-        // Compare the sorted arrays
-        return Arrays.equals(target, arr);
-    }
-}
